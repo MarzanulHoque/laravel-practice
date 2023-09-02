d91aae5e4a45f1f15ed85e60ab775d7d71269c60
marzanulhoque <marzanulru17@gmail.com>
2023-09-02 12:17:07 +0600

    Made a route and link to it

---
+++ My-Blog/resources/views/posts.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+    {{-- <script src="/app.js"></script> --}}
+</head>
+<body>
+    <article>
+        
+        <h1><a href="/post"> My First Post </a></h1>
+        
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quisquam fugit, natus voluptatem cum est possimus ipsa at perferendis nulla, temporibus velit minus, nemo excepturi consectetur repudiandae soluta quaerat? Sequi illo accusamus ad, aliquid assumenda eligendi harum, nobis, magnam quam in molestias! Vitae blanditiis veniam magnam nesciunt in consectetur autem aspernatur libero obcaecati omnis excepturi nihil est rerum ut, illum nemo animi optio. Eligendi qui quisquam tenetur facere ad assumenda fugiat dignissimos in labore tempore cum alias vitae illo architecto provident dolor repellat nemo, impedit distinctio, quod temporibus minus commodi quos? Minima exercitationem minus magnam, aut inventore quisquam repellendus! Placeat cupiditate ducimus obcaecati et sunt quidem iure, quis blanditiis, hic accusantium expedita molestias adipisci. Veniam debitis voluptate ratione ad quisquam?
+        </p>
+
+    </article>
+
+     <article>
+        
+        <h1><a href="/post"> My Second Post </a></h1>
+        
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quisquam fugit, natus voluptatem cum est possimus ipsa at perferendis nulla, temporibus velit minus, nemo excepturi consectetur repudiandae soluta quaerat? Sequi illo accusamus ad, aliquid assumenda eligendi harum, nobis, magnam quam in molestias! Vitae blanditiis veniam magnam nesciunt in consectetur autem aspernatur libero obcaecati omnis excepturi nihil est rerum ut, illum nemo animi optio. Eligendi qui quisquam tenetur facere ad assumenda fugiat dignissimos in labore tempore cum alias vitae illo architecto provident dolor repellat nemo, impedit distinctio, quod temporibus minus commodi quos? Minima exercitationem minus magnam, aut inventore quisquam repellendus! Placeat cupiditate ducimus obcaecati et sunt quidem iure, quis blanditiis, hic accusantium expedita molestias adipisci. Veniam debitis voluptate ratione ad quisquam?
+        </p>
+        
+    </article>
+
+     <article>
+
+        <h1><a href="/post"> My Third Post </a></h1>
+        
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quisquam fugit, natus voluptatem cum est possimus ipsa at perferendis nulla, temporibus velit minus, nemo excepturi consectetur repudiandae soluta quaerat? Sequi illo accusamus ad, aliquid assumenda eligendi harum, nobis, magnam quam in molestias! Vitae blanditiis veniam magnam nesciunt in consectetur autem aspernatur libero obcaecati omnis excepturi nihil est rerum ut, illum nemo animi optio. Eligendi qui quisquam tenetur facere ad assumenda fugiat dignissimos in labore tempore cum alias vitae illo architecto provident dolor repellat nemo, impedit distinctio, quod temporibus minus commodi quos? Minima exercitationem minus magnam, aut inventore quisquam repellendus! Placeat cupiditate ducimus obcaecati et sunt quidem iure, quis blanditiis, hic accusantium expedita molestias adipisci. Veniam debitis voluptate ratione ad quisquam?
+        </p>
+
+    </article>
+
+</body>
+</html>
