3fdb3773c9e76d530a257db541331d8ae541627e
marzanulhoque <marzanulru17@gmail.com>
2023-09-06 21:19:35 +0600

    Used Factories to create dummy data

--- My-Blog/database/seeders/DatabaseSeeder.php
+++ My-Blog/database/seeders/DatabaseSeeder.php
@@ -18,48 +18,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' =>'personal'
-
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' =>'family'
-
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' =>'work'
-
-        ]);
-
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id' => $family->id,
-            'title'=>'My Family Post',
-            'excerpt' => 'lorem ipsum dolor sit amet.',
-            'slug'=>'my-family-post',
-            'body'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis molestias fugit fuga quis eveniet iure? Possimus, corporis atque! Perspiciatis facere ullam similique delectus suscipit rerum quo numquam reprehenderit praesentium dignissimos veniam libero totam mollitia est, iusto debitis molestiae? Officiis ipsam veritatis, magni quam id totam commodi eum omnis recusandae placeat natus, inventore minus assumenda asperiores qui excepturi dolores labore quas corporis accusantium consequatur ratione? Temporibus, corrupti atque. Corrupti recusandae soluta saepe quis ab, reiciendis officiis hic veniam vel ut odit. Eaque quaerat, rerum vitae culpa fugit eum corporis obcaecati. Voluptates enim corrupti porro est voluptatibus provident reprehenderit! Quisquam, magnam cupiditate.'
-        ]);
-
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id' => $work->id,
-            'title'=>'My Work Post',
-            'excerpt' => 'lorem ipsum dolor sit amet.',
-            'slug'=>'my-work-post',
-            'body'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis molestias fugit fuga quis eveniet iure? Possimus, corporis atque! Perspiciatis facere ullam similique delectus suscipit rerum quo numquam reprehenderit praesentium dignissimos veniam libero totam mollitia est, iusto debitis molestiae? Officiis ipsam veritatis, magni quam id totam commodi eum omnis recusandae placeat natus, inventore minus assumenda asperiores qui excepturi dolores labore quas corporis accusantium consequatur ratione? Temporibus, corrupti atque. Corrupti recusandae soluta saepe quis ab, reiciendis officiis hic veniam vel ut odit. Eaque quaerat, rerum vitae culpa fugit eum corporis obcaecati. Voluptates enim corrupti porro est voluptatibus provident reprehenderit! Quisquam, magnam cupiditate.'
-        ]);
-
 
 
     }
