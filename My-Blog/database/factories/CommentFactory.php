3d42ac3e3796db8558f112cf06ebf4f9f3889b09
marzanulhoque <marzanulru17@gmail.com>
2023-09-13 15:57:18 +0600

    Made Comment Section Dynamic

--- My-Blog/database/factories/CommentFactory.php
+++ My-Blog/database/factories/CommentFactory.php
@@ -3,6 +3,10 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\User;
+use League\CommonMark\Node\Block\Paragraph;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
@@ -17,7 +21,10 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
+            'body' => $this->faker->paragraph()
+
         ];
     }
 }
